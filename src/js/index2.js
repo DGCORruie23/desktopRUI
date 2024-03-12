116b149d85a72e6ab91c4b29b5e6e7b95e9b7898
Ariana336 <116653898+Ariana336@users.noreply.github.com>
2024-03-12 14:34:11 -0600

    actualizacion fecha, hora e inputs

---
+++ src/js/index2.js
@@ -0,0 +1,18 @@
+function actualizarFechaHora() {
+    var fechaElemento = document.getElementById('fecha');
+    var horaElemento = document.getElementById('hora');
+
+    // Obtener la fecha y la hora actual utilizando moment.js
+    var fechaActual = moment().format('YYYY-MM-DD');
+    var horaActual = moment().format('HH:mm:ss');
+
+    // Mostrar la fecha y la hora en los elementos correspondientes
+    fechaElemento.textContent = fechaActual;
+    horaElemento.textContent = horaActual;
+}
+
+// Actualizar la fecha y la hora cada segundo
+setInterval(actualizarFechaHora, 1000);
+
+// Llamar a la función por primera vez para mostrar la fecha y la hora inicial
+actualizarFechaHora();
