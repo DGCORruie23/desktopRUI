36925a762ef5b01d39f12dcb9cb5a47ba1386e9b
DGCORruie23 <dgcor.rui23@gmail.com>
2024-05-28 20:05:00 -0600

    cambios en la lógica de la aplicación 28-05-2024

--- index.js
+++ index.js
@@ -1,97 +1,15 @@
-const { error } = require('console');
 const { app, BrowserWindow, ipcMain } = require('electron');
 const path = require('path');
 const sqlite3 = require('sqlite3').verbose();
+const checkInternetConnected = require('check-internet-connected');
+const { copyFileSync } = require('fs');
 
-let mainWindow;
-let db;
-let progressInterval;
-let secondWindow;
-let thirdWindow = null;
-let NacWindow;
-let FamWindow;
+let mainWindow, db, progressInterval, secondWindow, thirdWindow, NacWindow, FamWindow;
 let userExists = false;
 let userData = null;
 
-app.on('ready', () => {
-  mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      nodeIntegration: false,
-      enableRemoteModule: false,
-      contextIsolation: true,
-      preload: path.join(__dirname, 'preload.js')
-    }
-  });
-
-  const checkInternetConnected = require('check-internet-connected');
-
-    const config = {
-      timeout: 5000, //timeout connecting to each try (default 5000)
-      retries: 3,//number of retries to do before failing (default 5)
-      domain: 'google.com'//the domain to check DNS record of
-    }
-
-    checkInternetConnected(config)
-      .then(() => {
-        console.log("Connection available");
-
-        const dbPath = path.join(__dirname, 'mydb.db');
-        console.log('Ruta de la base de datos:', dbPath);
-        db = new sqlite3.Database(dbPath, (err) => {
-          if (err) {
-            console.error('Error al abrir la base de datos:', err);
-          } else {
-            console.log('Base de datos SQLite abierta correctamente.');
-          }
-        });
-      
-      
-        mainWindow.loadFile('./src/res/ventanas/splashScreen.html');
-        const INCREMENT = 0.03;
-        const INTERVAL_DELAY = 30; // ms
-      
-        let c = 0;
-        progressInterval = setInterval(async () => {
-          mainWindow.setProgressBar(c);
-      
-          if (c >= 0.25 && c < 0.25 + INCREMENT) {
-            await checkForUser();
-            if (userExists) {
-              // await updateUser(userData.nickname,userData.password)
-              // createThirdWindowWithUser(userData);
-              mainWindow.webContents.send('update-data', userData);
-
-
-            } else {
-              createSecondWindow();
-            }
-          }
-      
-          if (c < 1) {
-            c += INCREMENT;
-          } else {
-
-              clearInterval(progressInterval);
-              mainWindow.setProgressBar(-1);
-              mainWindow.hide();
-              mainWindow.close();
-
-          }
-        }, INTERVAL_DELAY);
-      }).catch((err) => {
-        console.log("No connection", err);
-
-
-
-
-
-
-
-
-
-          const dbPath = path.join(__dirname, 'mydb.db');
+function connectToDatabase() {
+  const dbPath = path.join(__dirname, 'mydb.db');
   console.log('Ruta de la base de datos:', dbPath);
   db = new sqlite3.Database(dbPath, (err) => {
     if (err) {
@@ -100,18 +18,37 @@ app.on('ready', () => {
       console.log('Base de datos SQLite abierta correctamente.');
     }
   });
+}
+
+function checkDataBase(){
+  const dbPath = path.join(__dirname, 'mydb.db');
+  console.log('Ruta de la base de datos:', dbPath);
+  found = sqlite3.NOTFOUND(dbPath, (err) => {
+    if(err){
+      console,error('No se encontró la base de datos: ', err)
+    } else{
+      console.log("Base de datos encontrada");
+    }
+  })
+}
 
 
+function loadMainWindow() {
   mainWindow.loadFile('./src/res/ventanas/splashScreen.html');
   const INCREMENT = 0.03;
   const INTERVAL_DELAY = 30; // ms
-
   let c = 0;
+
   progressInterval = setInterval(async () => {
     mainWindow.setProgressBar(c);
 
     if (c >= 0.25 && c < 0.25 + INCREMENT) {
       await checkForUser();
+      if (userExists) {
+        mainWindow.webContents.send('update-data', userData);
+      } else {
+        createSecondWindow();
+      }
     }
 
     if (c < 1) {
@@ -129,9 +66,38 @@ app.on('ready', () => {
       }
     }
   }, INTERVAL_DELAY);
-      });
+}
 
-      
+app.on('ready', () => {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: false,
+      enableRemoteModule: false,
+      contextIsolation: true,
+      preload: path.join(__dirname, 'preload.js')
+    }
+  });
+
+  const config = {
+    timeout: 10000,
+    retries: 3,
+    domain: 'ruie.dgcor.com'
+  };
+
+  checkInternetConnected(config)
+    .then(() => {
+      console.log("Connection available");
+      connectToDatabase();
+      loadMainWindow();
+    })
+    .catch((err) => {
+      console.log("No connection", err);
+      connectToDatabase();
+      loadMainWindow();
+    });
+
   ipcMain.once('validado', handleValidado);
 });
 
@@ -156,6 +122,8 @@ async function checkForUser() {
 }
 
 function createSecondWindow() {
+  if (secondWindow) return; // Evitar crear múltiples ventanas
+
   secondWindow = new BrowserWindow({
     width: 800,
     height: 600,
@@ -181,17 +149,13 @@ function createSecondWindow() {
   ipcMain.once('validado', handleValidado);
 }
 
-
-
-
 function handleValidado(event, validado) {
-  console.log("Lo que trae handleValidado")
-  console.log(validado);
+  console.log("Lo que trae handleValidado", validado);
   if (validado.val === 1) {
     if (secondWindow) {
       secondWindow.hide();
       secondWindow.close();
-      secondWindow = null;  // Asegúrate de liberar la referencia
+      secondWindow = null;
     }
     const userData = {
       nickname: validado.nickname,
@@ -207,9 +171,10 @@ function handleValidado(event, validado) {
 }
 
 function createThirdWindowWithUser(user) {
+  if (thirdWindow) return; // Evitar crear múltiples ventanas
+
   try {
     console.log("Creating third window with user data:", user);
-    
 
     thirdWindow = new BrowserWindow({
       width: 1366,
@@ -222,7 +187,6 @@ function createThirdWindowWithUser(user) {
       }
     });
 
-    console.log("Loading file './src/res/ventanas/captura.html'");
     thirdWindow.loadFile('./src/res/ventanas/captura.html');
 
     thirdWindow.on('closed', () => {
@@ -247,18 +211,12 @@ function createThirdWindowWithUser(user) {
   }
 }
 
-
-
-
-
-
-
-
 function handleNacionalidad(event, nac) {
   if (nac === 1) {
     if (thirdWindow) {
       thirdWindow.hide();
       thirdWindow.close();
+      thirdWindow = null; // Asegurar que se libera la referencia
     }
     createNacWindow();
   }
@@ -269,12 +227,15 @@ function handleFamilia(event, fam) {
     if (thirdWindow) {
       thirdWindow.hide();
       thirdWindow.close();
+      thirdWindow = null; // Asegurar que se libera la referencia
     }
     createFamWindow();
   }
 }
 
 function createNacWindow() {
+  if (NacWindow) return; // Evitar crear múltiples ventanas
+
   NacWindow = new BrowserWindow({
     width: 1366,
     height: 768,
@@ -305,12 +266,15 @@ function handleRetCapNac(event, retCaptura) {
     if (NacWindow) {
       NacWindow.hide();
       NacWindow.close();
+      NacWindow = null; // Asegurar que se libera la referencia
     }
     createThirdWindowWithUser(userData);
   }
 }
 
 function createFamWindow() {
+  if (FamWindow) return; // Evitar crear múltiples ventanas
+
   FamWindow = new BrowserWindow({
     width: 1366,
     height: 768,
@@ -341,6 +305,7 @@ function handleRetCapFam(event, retCaptura) {
     if (FamWindow) {
       FamWindow.hide();
       FamWindow.close();
+      FamWindow = null; // Asegurar que se libera la referencia
     }
     createThirdWindowWithUser(userData);
   }
@@ -374,7 +339,7 @@ app.on('before-quit', () => {
 
 app.on('activate', () => {
   if (BrowserWindow.getAllWindows().length === 0) {
-    createSecondWindow(); // O crea otra ventana principal según tu lógica
+    createSecondWindow();
   }
 });
 
@@ -385,76 +350,3 @@ app.on('certificate-error', (event, webContents, url, error, certificate, callba
 
 app.commandLine.appendSwitch('ignore-certificate-errors', 'true');
 app.enableSandbox();
-
-
-
-
-
-
-
-
-async function updateUser(nickname,password) {
-
-
-
-  // const data = JSON.stringify({
-  //     "nickname": nickname,
-  //     "password": password
-  // });
-
-  // try {
-  //   console.log("usuario actualice ");
-  //   console.log(data);
-  //     const response = await fetch("https://ruie.dgcor.com/login/validar/", {
-  //         method: 'POST',
-  //         headers: {
-  //             'Content-Type': 'application/json',
-  //             'User-Agent': 'insomnia/8.6.0'
-  //         },
-  //         body: data
-  //     });
-  //     console.log("entro aqui")
-  //     console.log(response);
-  //     if (response.ok) {
-  //         const responseData = await response.json();
-  //         const valPasswd = responseData['password'];
-  //         console.log(valPasswd);
-  //         console.log(responseData);
-  //         if (valPasswd === 'ok') {
-  //           //   validado = 1;
-  //           //   console.log("Usuario correcto");
-  //           //   console.log(validado);
-
-            
-  //           const nickname = responseData['nickname'];
-  //           const nombre = responseData['nombre'];
-  //           const apellido = responseData['apellido'];
-  //           const estado = responseData['estado'];
-  //           const tipo = responseData['tipo'];
-
-  //           const validado = {
-  //               nickname: nickname,
-  //               nombre: nombre,
-  //               apellido: apellido,
-  //               estado: estado,
-  //               tipo: tipo,
-  //               password: password,
-  //           };
-  //           console.log("insertando usuario....")
-  //           insertUser(nickname, nombre, apellido, password, estado, tipo);
-
-  //         } else {
-  //             validado = 0;
-  //             console.log("Hubo un cambio en la contraseña");
-  //             createSecondWindow();
-  //         }
-  //     } else {
-  //       console.log("pepe");
-  //       console.log(error);
-  //     }
-  // } catch (error) {
-  //     console.error('Error:', error);
-  // } finally {
-  // }
-  // validateUser();
-}
